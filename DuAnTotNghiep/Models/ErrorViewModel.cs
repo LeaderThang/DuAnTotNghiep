3ad863dc3344fc8e9f203c39ad5f45e323185d2f
Codelor34 <phucbhph32061@fpt.edu.vn>
2025-05-29 15:07:28 +0700

    1
    1

--- DuAnTotNghiep/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DuAnTotNghiep.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
